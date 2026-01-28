2d3957f02488e39e59699f81b92ad25896ea17be
RAZALI Ayoub <a-razali@enageo.com>
2026-01-28 15:49:45 +0100

    add animation really

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslocoDirective } from '@ngneat/transloco';
-import { LanguageSwitcherComponent } from "./components/language-switcher/language-switcher.component";
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { HeroComponent } from "./components/hero/hero.component";
-import { AboutMeComponent } from "./components/about-me/about-me.component";
-import { EducationComponent } from "./components/education/education.component";
-import { ExperienceComponent } from "./components/experience/experience.component";
-import { ProjectComponent } from "./components/project/project.component";
-import { CompetanceComponent } from "./components/competance/competance.component";
-import { ContactMeComponent } from "./components/contact-me/contact-me.component";
-
+import { LanguageSwitcherComponent } from './components/language-switcher/language-switcher.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectComponent } from './components/project/project.component';
+import { CompetanceComponent } from './components/competance/competance.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { YoutubeModalComponent } from './components/youtube-modal/youtube-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslocoDirective, LanguageSwitcherComponent, NavBarComponent, HeroComponent, AboutMeComponent, EducationComponent, ExperienceComponent, ProjectComponent, CompetanceComponent, ContactMeComponent],
+  imports: [
+    RouterOutlet,
+    TranslocoDirective,
+    LanguageSwitcherComponent,
+    NavBarComponent,
+    HeroComponent,
+    AboutMeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectComponent,
+    CompetanceComponent,
+    ContactMeComponent,
+    YoutubeModalComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   // Set your target date (for example: 1st June 2025)
 
-  ngOnInit() {
-   
-  }
-
+  ngOnInit() {}
 }
